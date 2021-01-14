83a10d3ec38f72e1c3ec9b128317bd5cadcf3843
IM5K58 <68091185+IM5K58@users.noreply.github.com>
2021-01-14 11:38:17 +0900

    Update script.js

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ var canvas = document.getElementById("remainTime");
 var ctx = canvas.getContext("2d");
 ctx.strokeStyle = "#f6ff00";
 ctx.fillStyle = 'red';
-ctx.lineWidth = 200;
+ctx.lineWidth = 50;
 
 var darkMode = $("#darkMode");
 var lightMode = $("#lightMode");
